b4a7d8b2beaa3db55287f3b6392f319c763df018
Breno M <breno_marino@hotmail.com>
2020-10-14 18:21:56 -0300

    project start

---
+++ src/main/java/br/com/tijo/api/mail/dto/ApiMailDtoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tijo.api.mail.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMailDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMailDtoApplication.class, args);
+	}
+
+}
